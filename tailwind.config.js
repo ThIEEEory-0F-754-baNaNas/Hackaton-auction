d810f146b6913be46379d5d617507fb204280718
erotourtes <erotourtes@gmail.com>
2024-02-06 16:07:12 +0200

    Feat: added material-tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,15 @@
+import withMT from "@material-tailwind/react/utils/withMT";
+
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+export default withMT({
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "var(--color-primary)",
+        // TODO: Map colors from the index.css
+      },
+    },
   },
   plugins: [],
-}
-
+});
