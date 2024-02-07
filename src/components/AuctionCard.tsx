7121c85ef0873b786e4a1ad31a09fa9d3b47c507
erotourtes <erotourtes@gmail.com>
2024-02-07 16:13:42 +0200

    WIP: carousel

--- src/components/AuctionCard.tsx
+++ src/components/AuctionCard.tsx
@@ -10,7 +10,7 @@ import React from "react";
 
 const AuctionCard = () => {
   return (
-    <Card className="w-[500px]">
+    <Card className="w-[500px] min-w-[500px]">
       <CardHeader floated={false} className="relative h-56">
         <img
           src="https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80"
