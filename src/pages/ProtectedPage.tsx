975efe8f38931f3ec30193c67422fcab4f8076e7
erotourtes <erotourtes@gmail.com>
2024-02-10 09:44:30 +0200

    WIP: manually delaying waiting for user

--- src/pages/ProtectedPage.tsx
+++ src/pages/ProtectedPage.tsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Navigate, Outlet } from "react-router-dom";
 import { SIGN_IN } from "../Navigation";
 import { User } from "../api/userApi";
+import { Spinner } from "@material-tailwind/react";
 
 interface Props {
   user: User | null;
@@ -10,6 +11,23 @@ interface Props {
 }
 
 const ProtectedPage = ({ user, redirectTo = SIGN_IN, children }: Props) => {
+  const [isLoading, setLoading] = useState(true);
+
+  // TODO: use real loading
+  useEffect(() => {
+    if (user) {
+      setLoading(false);
+    }
+
+    setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+  }, [user]);
+
+  if (isLoading) {
+    return <Spinner />;
+  }
+
   if (!user) {
     return <Navigate to={redirectTo} replace />;
   }
