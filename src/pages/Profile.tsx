57fe9500f87b235482a123dab8d911ee8121e1cb
erotourtes <erotourtes@gmail.com>
2024-02-11 23:54:24 +0200

    Fix: layout

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,12 +1,23 @@
-import UploadFile from "../components/UploadFile";
-import { updateProfileAvatar } from "../api/userApi";
+import { Button, Input } from "@material-tailwind/react";
 import { useContext, useState } from "react";
-import { UserContext } from "../context/userContext";
+import { addDeposit, updateProfileAvatar } from "../api/userApi";
 import ErrorIndicator from "../components/ErrorIndicator";
+import UploadFile from "../components/UploadFile";
+import { UserContext } from "../context/userContext";
 
 const Profile = () => {
-  const [, setUser] = useContext(UserContext);
+  const [user, setUser] = useContext(UserContext);
   const [error, setError] = useState<string | null>(null);
+
+  const [depositAmount, setDepositAmount] = useState(0);
+
+  const handleAddDeposit = async () => {
+    await addDeposit(depositAmount);
+    if (user.isNotOk) throw new Error();
+    setUser({ ...user, balance: user.balance + depositAmount });
+  };
+
+  if (user.isNotOk) return <ErrorIndicator error={user} />;
 
   return (
     <div>
@@ -20,6 +31,17 @@ const Profile = () => {
         label={"Update avatar"}
       />
       {error && <ErrorIndicator error={error} />}
+
+      <div className="flex">
+        <Input
+          type="number"
+          placeholder={depositAmount.toString()}
+          onChange={(e) => setDepositAmount(Number(e.target.value))}
+          label="Add Deposit"
+          crossOrigin={undefined}
+        />
+        <Button onClick={handleAddDeposit}>Add Deposit</Button>
+      </div>
     </div>
   );
 };
