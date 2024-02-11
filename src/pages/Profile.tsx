fec34d12c219ce9914cd89d853df61438eb767a0
erotourtes <erotourtes@gmail.com>
2024-02-11 22:04:16 +0200

    Fix: user profile reflects changes on icon upload

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,10 +1,19 @@
 import UploadFile from "../components/UploadFile";
 import { updateProfileAvatar } from "../api/userApi";
+import { useContext } from "react";
+import { UserContext } from "../context/userContext";
 
 const Profile = () => {
+  const [, setUser] = useContext(UserContext);
+
   return (
     <div>
-      <UploadFile onUpload={updateProfileAvatar} label={"Update avatar"} />
+      <UploadFile
+        onUpload={(file) => {
+          updateProfileAvatar(file).then((user) => setUser(user));
+        }}
+        label={"Update avatar"}
+      />
     </div>
   );
 };
